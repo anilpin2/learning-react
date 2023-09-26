748cfee5ada09b49fc585308b4883000408e0005
Anil <anil.pin2@gmail.com>
2023-09-26 23:18:39 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,7 @@
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("h1", {}, "I am h1 Tag!")
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
